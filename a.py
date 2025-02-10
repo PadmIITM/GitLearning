235b89bcea687dd4a6581d4055c61507cbdae47d
PadmIITM <22f3000833@ds.study.iitm.ac.in>
2025-02-10 21:45:12 +0530

    asadasd

---
+++ a.py
@@ -0,0 +1,16 @@
+
+checking availale keys:-
+ls -al ~/.ssh
+
+create key:
+ssh-keygen -t ed25519 -C "your-email@example.com"
+
+#commit pushin command 
+git push origin main
+
+add agent:
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519  # or ~/.ssh/id_rsa
+
+access key
+cat ~/.ssh/id_ed25519.pub  # or ~/.ssh/id_rsa.pub
